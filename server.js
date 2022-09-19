f1b9c3bb685e08bcbbb3c53ffe46a7a0a403168e
Laceinstein <laceinstein@gmail.com>
2022-09-19 14:42:38 +0200

    remove purpose comment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/prod')))
 
-// this * route is to serve project on different page routes except root `/` for upgrade purpose
+// this * route is to serve project on different page routes except root `/`
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/prod/index.html'))
 })
